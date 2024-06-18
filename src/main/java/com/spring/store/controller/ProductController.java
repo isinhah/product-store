cb1d0a609913e0f529b25751e61a5f4daebad6b8
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-06-18 07:29:28 -0300

    formulario para criar produto

--- src/main/java/com/spring/store/controller/ProductController.java
+++ src/main/java/com/spring/store/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.spring.store.controller;
 
 import com.spring.store.model.Product;
+import com.spring.store.model.ProductDto;
 import com.spring.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,5 +24,12 @@ public class ProductController {
         List<Product> products = productRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "products/CreateProduct.html";
     }
 }
