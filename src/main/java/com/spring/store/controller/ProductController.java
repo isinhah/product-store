1fddf29b3c04014bcc84b59667c417dd1a88a079
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-06-18 11:56:03 -0300

    atualizar produto existente

--- src/main/java/com/spring/store/controller/ProductController.java
+++ src/main/java/com/spring/store/controller/ProductController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -87,6 +84,80 @@ public class ProductController {
         product.setImageFileName(storageFileName);
 
         productRepository.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam int id) {
+
+        try {
+            Product product = productRepository.findById(id).get();
+            model.addAttribute("product", product);
+
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto", productDto);
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+            return "redirect:/products";
+        }
+
+        return "products/EditProduct";
+    }
+
+    @PostMapping("/edit")
+    public String updateProduct(Model model, @RequestParam int id, @Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+
+        try {
+            Product product = productRepository.findById(id).get();
+            model.addAttribute("product", product);
+
+            if (result.hasErrors()) {
+                return "products/EditProduct";
+            }
+
+            if (productDto.getImageFile() != null && !productDto.getImageFile().isEmpty()) {
+                // Deleta imagem antiga
+                String uploadDir = "public/images/";
+                Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+
+                try {
+                    Files.delete(oldImagePath);
+                } catch (Exception ex) {
+                    System.out.println("Exception: " + ex.getMessage());
+                }
+
+                // salva a nova imagem
+                MultipartFile image = productDto.getImageFile();
+                Date createdAt = new Date();
+                String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+                try (InputStream inputStream = image.getInputStream()) {
+                    Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                            StandardCopyOption.REPLACE_EXISTING);
+                }
+
+                product.setImageFileName(storageFileName);
+            }
+
+            product.setName(productDto.getName());
+            product.setBrand(productDto.getBrand());
+            product.setCategory(productDto.getCategory());
+            product.setPrice(productDto.getPrice());
+            product.setDescription(productDto.getDescription());
+
+            productRepository.save(product);
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
 
         return "redirect:/products";
     }
